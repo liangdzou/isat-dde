b171a284cae8fe16b006e85433de205571405077
liangz <liangz@outlook.com>
2014-12-23 14:55:08 +0800

    poly Lyapunov

--- matlabFunctions/poly_Lyapunov_backup.m
+++ matlabFunctions/poly_Lyapunov_backup.m
@@ -1,69 +1,83 @@
-function [V,dm] = poly_Lyapunov_backup(coefBefore, coefAfter, xi, varNums, deg, coefBL, coefBU, delta)
+function [V,dm,exitflag] = poly_Lyapunov(coefBefore, coefAfter, xi, varNums, deg, coefBL, coefBU, delta, upsilon)
 
-if varNums == 1 
-    funcStr = '@(cb1';
-    for i = 2 : deg + 1
-        funcStr = strcat(funcStr, ',cb', num2str(i));
-    end
-    funcStr = strcat(funcStr, ',xi1,xia1)');
-else
-    funcStr = '@(';
-    for i = 1 : varNums
-        for j = 1 : deg + 1
-            funcStr = strcat(funcStr, 'cb', num2str(i), '_', num2str(j), ',');
-        end
-    end
-    for i = 1 : varNums
-        funcStr = strcat(funcStr, 'xi', num2str(i), ',');
-    end
-    for i = 1 : varNums - 1
-        funcStr = strcat(funcStr, 'xia', num2str(i), ',');
-    end
-    funcStr = strcat(funcStr, 'xia', num2str(varNums), ')');
-end
+%calculate an0, an1, xi0, xi1, ep, Ico
 
-coefVector = [transpose(coefBefore);xi];
-coefVector = coefVector(:);
-varSize = size(coefVector,1);
-P = sym('polyP',[varSize, varSize]);
 
-xi1 = sym('xia',size(xi));
-coefVector1 = [transpose(coefAfter);xi1];
-coefVector1 = coefVector1(:);
-
-% funcStr = strcat(funcStr, ' @(');        
-% for i = 1 : varSize
-%     for j = 1 : varSize
-%         funcStr = strcat(funcStr, 'polyP', num2str(i), '_', num2str(j));
-%         if i ~= varSize || j ~= varSize
-%             funcStr = strcat(funcStr, ',');
-%         end
-%     end
-% end
-% funcStr = strcat(funcStr, ')');
-    
-% V = eval([funcStr, vectorize(transpose(coefVector)*P*coefVector)]);
-% V1 = eval([funcStr, vectorize(transpose(coefVector1)*P*coefVector1)]);
-expr1 = eval([funcStr, vectorize(transpose(coefVector1)*P*coefVector1 - transpose(coefVector)*P*coefVector)]);
-expr = eval([funcStr, '[', vectElementsString(coefVector), ']']);
-
+% calculate coefBFMat
 coefBLVect = ones(1, varNums*(deg+1)) * coefBL;
 coefBUVect = ones(1, varNums*(deg+1)) * coefBU;
-xiBLVect = zeros(1,2*varNums);
-xiBUVect = ones(1,2*varNums) * delta;
+xiBLVect = zeros(1,varNums);
+xiBUVect = ones(1,varNums) * delta;
 BLVect = [coefBLVect, xiBLVect];
 BUVect = [coefBUVect, xiBUVect];
 varInter = interval(BLVect, BUVect);
+coefBFMat = transpose(varInter) * varInter;
 
-predStr = 'expr(varInter(1)';
+% calculate coefAFMat
+xi1 = sym('xia', varNums);
+coefParameters = [transpose(coefBefore); xi; xi1];
+coefParameters= coefParameters(:);
+coefAFVector = [transpose(coefAfter); xi1];
+coefAFVector = coefAFVector(:);
+coefAF = eval(['@(', strrep(strrep(vectElementsString(coefParameters),'[',''),']',''), ') [', vectElementsString(coefAFVector), ']']);
+varAFParameters = [varInter, interval(xiBLVect, xiBUVect)];
+coefAFStr = 'coefAF(varAFParameters(1)';
 for i = 2 : varNums*(deg + 3)
-    predStr = strcat(predStr, ', varInter(', num2str(i), ')');
+    coefAFStr = strcat(coefAFStr, ', varAFParameters(', num2str(i), ')');
 end
-predStr = strcat(predStr, ')');
-pred = eval(predStr);
+coefAFStr = strcat(coefAFStr, ')');
+coefAFInter = eval(coefAFStr);
+coefAFMat = transpose(coefAFInter)*coefAFInter;
 
-V = expr;
-dm = pred;
+% calculate coefMat, and call poly_lp
+coefMat = coefBFMat - coefAFMat;
+
+% calculate bigc
+
+
+set(0,'RecursionLimit',10000);
+
+coefMatBL = coefMat.inf;
+coefMatBU = coefMat.sup;
+coefVectBL = transpose(coefMatBL(:));
+coefVectBU = transpose(coefMatBU(:));
+
+% linear programming
+f = [zeros(2*size(coefVectBL,2), 1); -1];
+A = [-coefVectBL, coefVectBU, 1];
+b = 0;
+lb = zeros(2*size(coefVectBL,2) + 1, 1);
+ub = coefBU * ones(2*size(coefVectBL,2) + 1, 1);
+[x,fval,exitflag] = linprog(f,A,b,[],[],lb,ub);
+
+times = 0;
+while (fval >= 0 || max(A*x) > 0 || exitflag <= 0)
+    [maxRow,maxIndeices] = max(coefVectBU - coefVectBL);
+    [val,j] = max(maxRow);
+    i = maxIndeices(j);
+    
+    times = times + 1;
+    if mod(times, 10) == 0
+        times = times;
+    end
+    
+    if val > upsilon
+        rowSize = size(coefVectBL,1);
+        coefVectBL = [coefVectBL; coefVectBL(i,:)];
+        coefVectBU = [coefVectBU; coefVectBU(i,:)];
+        valMid = (coefVectBL(i,j) + coefVectBU(i,j))/2;
+        coefVectBU(i,j) = valMid;
+        coefVectBL(rowSize + 1,j) = valMid;
+        A = [- coefVectBL, coefVectBU, ones(rowSize + 1,1)];
+        b = zeros(rowSize + 1,1);
+        [x,fval,exitflag] = linprog(f,A,b,[],[],lb,ub);
+    else
+        break;
+    end
+end
+
+dm = -fval;
+V = 'not implemented';
 
 end
 
--- matlabFunctions/vectElementsString.m
+++
@@ -1,8 +0,0 @@
-function elementsStr = vectElementsString(vect)
-
-vectStr = vectorize(vect);
-i = strfind(vectStr, '[[');
-j = strfind(vectStr, '])');
-elementsStr = vectStr(i+1 : j-1);
-
-end

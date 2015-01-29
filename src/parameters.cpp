019aa6a9dfb9e05914378ef76070ce093ab102b8
liangz <liangz@outlook.com>
2015-01-29 23:13:45 +0800

    small change

---
+++ src/parameters.cpp
@@ -0,0 +1,17 @@
+#include "parameters.h"
+#include <string>
+using std::string;
+
+string para::file("");
+
+double para::a_w = 1000.0;
+double para::co_w = 1000;
+double para::e = 0.01;
+double para::md = 0.01;
+
+long para::expr_t_max = 6000000000;
+long para::bmc_t_max = 6000000000;
+
+double para::c_min = -1000;
+double para::c_max = 1000;
+double para::c_delta = 0.1;

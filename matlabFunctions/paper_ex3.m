59084b8b06ad0c588db8a262d4061c9d7b052c5f
liangz <liangz@outlook.com>
2015-02-07 16:24:00 +0800

    small change

--- matlabFunctions/paper_ex3.m
+++ matlabFunctions/paper_ex3.m
@@ -1,14 +1,14 @@
  clear;
 
 % inputs (4-tuple)
-DDES = @(x) -1/10*x*x*x;
+DDES = @(x) -x*x*x;
 LINEAR = 0;
-INIT = 1;
+INIT = 10;
 DELTA = 1;
 
 % parameters
 DEG = 1;
 
 % target (variables are x1, ..., xn)
-TARGET = [sym('x1>1'); sym('x1<-1')];
+TARGET = [sym('x1>20'); sym('x1<-20')];
 
